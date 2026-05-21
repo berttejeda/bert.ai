36bb33230ebd1502e64bb07bb7a8a618799328a5
Bert Tejeda <berttejeda@gmail.com>
2026-05-21 10:57:32 -0400

    Updated site for new correct BaseURL

--- assets/slidev/section-g0yfgt9l.js
+++
@@ -1,1 +0,0 @@
-import{Q as t,h as s,e as o,Y as a}from"../modules/vue-DvMVvspN.js";import{u as r}from"./context-M3TD9pkQ.js";const c={class:"slidev-layout section w-full h-full grid"},l={class:"my-auto text-center"},u={__name:"section",setup(n){return r(),(e,i)=>(t(),s("div",c,[o("div",l,[a(e.$slots,"default")])]))}};export{u as _};
