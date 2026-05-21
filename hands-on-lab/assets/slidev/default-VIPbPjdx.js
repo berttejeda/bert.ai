18b97f270254ef6582d2d7c669e3f6c1f1ab5096
Bert Tejeda <berttejeda@gmail.com>
2026-05-21 12:06:58 -0400

    Adjust base url

---
+++ hands-on-lab/assets/slidev/default-VIPbPjdx.js
@@ -0,0 +1,1 @@
+import{Q as t,h as o,Y as s}from"../modules/vue-DvMVvspN.js";import{b as r}from"../index-Oez9DmH_.js";const c={},a={class:"slidev-layout default"};function n(e,l){return t(),o("div",a,[s(e.$slots,"default")])}const f=r(c,[["render",n]]);export{f as I};
